cf778cfff288eee49770a2e40fe1b0ee337008c6
sgrG24 <sagar.gupta@avalara.com>
2020-12-20 15:15:03 +0530

    Adding basic structure of App
    Adding Burger, buildControls, buildControl React components

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import Aux from '../../hoc/Aux';
+import classes from './Layout.module.css';
+
+const layout = (props) => {
+    return (
+    <Aux>
+        <div>Toolbar, Sidedrawer, Backdrop</div>
+        <main className={classes.Content}>
+            {props.children}
+        </main>
+    </Aux>
+    );
+    
+}
+
+export default layout;
